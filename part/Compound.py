975eac70c187387d8f52d576a2abb0605886465f
Dan <link.dohi@gmail.com>
2015-02-01 01:53:48 -0800

    Added compounded parser
    Uses regex voodoo to figure out constituent elements of compounds.

--- part/Compound.py
+++ part/Compound.py
@@ -1,5 +1,20 @@
+from chemlib import getMolarMass as ptable_getMolarMass
+from re import findall, search
 
 
 class Compound():
 	def __init__(self, compound_string):
-		pass
+		self.elements = {}
+		raw_elements = findall("[A-Z][a-z]?[a-z]?[1-9]?[0-9]?", compound_string)
+		for element in raw_elements:
+			if element[-1].isdigit():
+				self.elements[search("[A-Z][a-z]?[a-z]?", element).group(0)] = int(search("[1-9][0-9]?", element).group(0))
+			else:
+				self.elements[search("[A-Z][a-z]?[a-z]?", element).group(0)] = 1
+		print self.elements
+
+	def getMolarMass(self):
+		mm = 0.0
+		for element in self.elements.keys():
+			mm += ptable_getMolarMass(element) * self.elements[element]
+		return mm
