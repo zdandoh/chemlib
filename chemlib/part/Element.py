6a4ac9bbaeb1872ff1dbc581e19990dcacbd01a7
Daniel Dohnalek <link.dohi@gmail.com>
2016-09-05 19:51:03 -0700

    Updated ptable contents
    - Ptable data stored in tuples instead of strings. Should improve performance
    - Converted tabs to spaces

--- chemlib/part/Element.py
+++ chemlib/part/Element.py
@@ -2,5 +2,5 @@ from re import search
 
 
 class Element():
-	def __init__(self, element_string):
-		self.count = search("[0-9][1-9]", element_string)
+    def __init__(self, element_string):
+        self.count = search("[0-9][1-9]", element_string)
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import os
 from setuptools import setup
 
 setup(
-	name = "chemlib",
-	version = "0.1",
-	description = "A lightweight library for chemistry",
-	author = "linkey11",
-	author_email = "link.dohi@gmail.com",
-	license = "MIT",
+    name="chemlib",
+    version="0.1.1",
+    description="A lightweight library for chemistry",
+    author="zdandoh",
+    author_email="link.dohi@gmail.com",
+    license="MIT",
 
-	packages = ["chemlib", os.path.join("chemlib", "part")]
-	)
+    packages=["chemlib", os.path.join("chemlib", "part")]
+)
